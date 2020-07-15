27a94f361880b4ad0d6362a4dbfdc5ab4547188c
Natan Ventura <natanvent@gmail.com>
2020-07-15 12:48:56 -0300

    Service Layer

---
+++ src/main/java/com/natan/demo/services/UserService.java
@@ -0,0 +1,27 @@
+package com.natan.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natan.demo.entities.User;
+import com.natan.demo.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
