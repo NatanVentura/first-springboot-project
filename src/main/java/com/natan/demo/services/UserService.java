037ea3a673a011a6ed952154f06279424a958692
Natan Ventura <natanvent@gmail.com>
2020-07-22 10:54:45 -0300

    Exception handling - delete

--- src/main/java/com/natan/demo/services/UserService.java
+++ src/main/java/com/natan/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.natan.demo.entities.User;
 import com.natan.demo.repositories.UserRepository;
+import com.natan.demo.services.exeptions.DatabaseException;
 import com.natan.demo.services.exeptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
